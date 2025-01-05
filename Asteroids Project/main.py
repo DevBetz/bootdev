c285b71d8924c1628fcd4b46768c5585a83e9f6d
DevBetz <176943749+DevBetz@users.noreply.github.com>
2025-01-05 11:51:11 -0800

    Add files via upload

--- Asteroids Project/main.py
+++ Asteroids Project/main.py
@@ -1,6 +1,3 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
 
@@ -10,21 +7,43 @@ from constants import *
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((1280, 720))
 
+    screen = pygame.display.set_mode((1280, 720))    
     pygame.display.set_caption("Asteroids Game")
 
-    running = True
-    while running:
-        screen.fill((0, 0, 0))
-        
-        pygame.display.update()
+    clock = pygame.time.Clock()
+    framerate = 60
 
+    running = True
+
+
+# get rid of this later, this is for testing:
+#    previous_time = pygame.time.get_ticks()
+
+
+    while running:
+# Testing Stuff        
+# Calculate dt (time difference since the last frame)
+        # current_time = pygame.time.get_ticks()
+        # dt = (current_time - previous_time) / 1000.0  # Convert to seconds
+        # previous_time = current_time  # Update previous_time for the next frame
+
+        # # Optional: Print dt to see how much time has passed (testing)
+        # print(f"Delta Time (dt): {dt:.4f} seconds")
+
+
+# End of Testing Stuff
+
+        screen.fill((0, 0, 0))
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                running =  False
                 return
 
-        
         pygame.display.flip()
+
+        clock.tick(framerate)
+
+        
 main()
